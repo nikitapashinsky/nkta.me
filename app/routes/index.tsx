760e979a0f7ee684322f27e394e07348b87f426a
Nikita <nikitapashinsky@gmail.com>
2023-02-21 19:13:10 +0300

    increase contrast of dark mode link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
             In the meantime, email me at{" "}
             <a
               href="mailto:n@nkta.me"
-              className="underline decoration-neutral-200 decoration-2 underline-offset-4 transition-all hover:text-sky-600 hover:decoration-sky-500 hover:underline-offset-[6px] dark:decoration-neutral-800 dark:hover:text-sky-400 dark:hover:decoration-sky-400"
+              className="underline decoration-neutral-200 decoration-2 underline-offset-4 transition-all hover:text-sky-600 hover:decoration-sky-500 hover:underline-offset-[6px] dark:decoration-neutral-700 dark:hover:text-sky-400 dark:hover:decoration-sky-400"
             >
               n@nkta.me
             </a>
