dce66138986353d72213dff1bd45ed55eada8c11
Nikita <nikitapashinsky@gmail.com>
2023-02-22 21:17:04 +0300

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Nikita Pashinsky</title>
+	<meta name="description" content="Nikita Pashinsky's personal website" />
+</svelte:head>
+
+<div class="min-h-full flex flex-col justify-between">
+	<div class="space-y-3">
+		<p class="text-sm">
+			<a
+				href="mailto:n@nkta.me"
+				class="text-neutral-500 underline underline-offset-[3px] decoration-neutral-700 hover:text-inherit hover:decoration-neutral-500 transition-colors"
+				>n@nkta.me</a
+			>
+		</p>
+		<p class="text-sm">
+			<a
+				href="https://twitter.com/nikitapashinsky"
+				class="text-neutral-500 underline underline-offset-[3px] decoration-neutral-700 hover:text-inherit hover:decoration-neutral-500 transition-colors"
+				>@nikitapashinsky</a
+			>
+		</p>
+	</div>
+
+	<p class="text-sm text-neutral-500">Every day is a small lifetime. Have a good life today!</p>
+</div>
