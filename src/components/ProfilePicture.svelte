007dc75d03dab84dfc991a4b74f85a51b2ba48ba
Nikita Pashinsky <nikitapashinsky@gmail.com>
2024-09-10 17:09:03 +0200

    upgrade to svelte 5

---
+++ src/components/ProfilePicture.svelte
@@ -0,0 +1,13 @@
+<script>
+	let count = $state(0);
+	function onclick() {
+		count++;
+	}
+</script>
+
+<button
+	{onclick}
+	class="flex h-6 w-6 items-center justify-center rounded-full bg-neutral-200 text-xs"
+>
+	{count}
+</button>
--- svelte.config.js
+++ svelte.config.js
@@ -2,4 +2,7 @@ import { vitePreprocess } from '@astrojs/svelte';
 
 export default {
 	preprocess: vitePreprocess(),
-}
+	compilerOptions: {
+		runes: true,
+	},
+};
