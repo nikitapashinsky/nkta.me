5bc6c4e92246c831b4620e240fb9a301db386208
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-02-21 16:22:32 +0100

    Add Copy email button

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,4 +1,5 @@
 interface SocialLink {
+	icon: string;
 	link: string;
 	title: string;
 	subtitle: string;
@@ -6,26 +7,31 @@ interface SocialLink {
 
 export const socials: SocialLink[] = [
 	{
+		icon: 'Email',
 		link: `mailto:nikitapashinsky@gmail.com`,
-		title: 'Email',
+		title: 'nikitapashinsky@gmail.com',
 		subtitle: 'Send me an email',
 	},
 	{
+		icon: 'Bluesky',
 		link: 'https://bsky.app/profile/nkta.me',
 		title: 'Bluesky',
 		subtitle: 'My only social media',
 	},
 	{
+		icon: 'CV',
 		link: 'https://read.cv/nkta',
 		title: 'CV',
 		subtitle: 'My work experience',
 	},
 	{
+		icon: 'Figma',
 		link: 'https://www.figma.com/@nkta',
 		title: 'Figma',
 		subtitle: 'Sharing free resources',
 	},
 	{
+		icon: 'Github',
 		link: 'https://github.com/nikitapashinsky',
 		title: 'Github',
 		subtitle: 'Learning to code in public',
