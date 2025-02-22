887ed4378cf090971a2acbb00f8e29dc4747d6eb
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-02-22 12:26:40 +0100

    Simplify social links

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,6 +2,7 @@ interface SocialLink {
 	icon: string;
 	link: string;
 	title: string;
+	handle?: string;
 	subtitle: string;
 }
 
@@ -9,31 +10,35 @@ export const socials: SocialLink[] = [
 	{
 		icon: 'Email',
 		link: `mailto:nikitapashinsky@gmail.com`,
-		title: 'nikitapashinsky@gmail.com',
+		title: 'Email',
 		subtitle: 'Send me an email',
 	},
 	{
 		icon: 'Bluesky',
 		link: 'https://bsky.app/profile/nkta.me',
 		title: 'Bluesky',
+		handle: '@nkta.me',
 		subtitle: 'My only social media',
 	},
 	{
 		icon: 'CV',
 		link: 'https://read.cv/nkta',
 		title: 'CV',
+		handle: '/nkta',
 		subtitle: 'My work experience',
 	},
 	{
 		icon: 'Figma',
 		link: 'https://www.figma.com/@nkta',
-		title: 'Figma',
+		title: 'Figma Community',
+		handle: '@nkta',
 		subtitle: 'Sharing free resources',
 	},
 	{
 		icon: 'Github',
 		link: 'https://github.com/nikitapashinsky',
 		title: 'Github',
+		handle: '/nikitapashinsky',
 		subtitle: 'Code for this site and other stuff',
 	},
 ];
