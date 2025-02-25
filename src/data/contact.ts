1f46a8c4441abc7207d18849033db860fc9f773e
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-02-25 19:09:49 +0100

    Removed some things, went back to Inter for the intro text

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -3,7 +3,6 @@ interface SocialLink {
 	link: string;
 	title: string;
 	handle?: string;
-	subtitle: string;
 }
 
 export const socials: SocialLink[] = [
@@ -11,34 +10,29 @@ export const socials: SocialLink[] = [
 		icon: 'Email',
 		link: `mailto:nikitapashinsky@gmail.com`,
 		title: 'Email',
-		subtitle: 'Send me an email',
 	},
 	{
 		icon: 'Bluesky',
 		link: 'https://bsky.app/profile/nkta.me',
 		title: 'Bluesky',
 		handle: '@nkta.me',
-		subtitle: 'My only social media',
 	},
 	{
 		icon: 'CV',
 		link: 'https://read.cv/nkta',
 		title: 'CV',
 		handle: '/nkta',
-		subtitle: 'My work experience',
 	},
 	{
 		icon: 'Figma',
 		link: 'https://www.figma.com/@nkta',
-		title: 'Figma Community',
+		title: 'Figma',
 		handle: '@nkta',
-		subtitle: 'Sharing free resources',
 	},
 	{
 		icon: 'Github',
 		link: 'https://github.com/nikitapashinsky',
 		title: 'Github',
 		handle: '/nikitapashinsky',
-		subtitle: 'Code for this site and other stuff',
 	},
 ];
