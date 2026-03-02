adc62c96393fed03815e4cc8bb4150de2798d6ac
Nikita Pashinsky <nikitapashinsky@gmail.com>
2026-03-02 23:58:17 +0100

    add popover

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Popover } from '@base-ui/react';
 import { cn } from '~/lib/utils';
 import { useTime } from '~/hooks/useTime';
 
@@ -27,7 +28,59 @@ export function Footer({ breakpoint }: { breakpoint: 'desktop' | 'mobile' }) {
 						{minutes}
 					</div>
 				</div>
-				here in Scheveningen.
+				here in{' '}
+				<Popover.Root>
+					<Popover.Trigger className={'group outline-none'}>
+						<span
+							className={cn(
+								'underline decoration-black/30 decoration-dotted decoration-1 underline-offset-[3px]',
+								'cursor-help outline-none',
+								'group-hover:text-black group-focus-visible:text-black group-data-popup-open:text-black',
+							)}
+						>
+							Scheveningen
+						</span>
+						.
+					</Popover.Trigger>
+					<Popover.Portal>
+						<Popover.Positioner side="top" sideOffset={8} positionMethod="fixed">
+							<Popover.Popup
+								className={cn(
+									'flex max-w-72 flex-col',
+									'rounded-md bg-white shadow-2xl ring-[0.5px] ring-black/8 outline-none',
+									'origin-(--transform-origin) transition-all duration-150 ease-out',
+									'data-starting-style:scale-95 data-starting-style:opacity-0',
+									'data-ending-style:scale-95 data-ending-style:opacity-0',
+								)}
+							>
+								<div
+									className={
+										'flex w-full snap-x snap-mandatory scroll-px-1 gap-1 overflow-x-auto px-1 pt-1 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden'
+									}
+								>
+									<div
+										className={
+											'aspect-4/3! w-full shrink-0 snap-start snap-always rounded-xs bg-[url(/images/scheveningen.webp)] bg-cover'
+										}
+									/>
+									{/* <div
+										className={
+											'h-full w-64 shrink-0 snap-center snap-always rounded-xs bg-neutral-200'
+										}
+									/>
+									<div
+										className={
+											'h-full w-64 shrink-0 snap-end snap-always rounded-xs bg-neutral-300'
+										}
+									/> */}
+								</div>
+								<div className={'p-3 text-sm font-[450] lg:text-xs'}>
+									A seaside district in The Hague, Netherlands.
+								</div>
+							</Popover.Popup>
+						</Popover.Positioner>
+					</Popover.Portal>
+				</Popover.Root>
 			</div>
 		</div>
 	);
