53bc6b4ab279063f074ff85536fa1a554ed3eb9d
Nikita Pashinsky <nikitapashinsky@gmail.com>
2026-03-14 00:21:11 +0100

    fix react/tanstack plugins mixup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite-plus";
 import path from "path";
-// import react from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 const root = path.resolve(__dirname, "src");
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": root,
