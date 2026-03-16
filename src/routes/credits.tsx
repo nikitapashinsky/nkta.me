4c68a3da6045dbd60e82f2597eb057f2307bf5ed
Nikita Pashinsky <nikitapashinsky@gmail.com>
2026-03-16 15:55:57 +0100

    update credits page
    - "Tailwind Merge" -> "tailwind-merge"
    - Reduce icon size
    - Reorganize links
    - Add subheading / summary

--- src/routes/credits.tsx
+++ src/routes/credits.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { twMerge } from 'tailwind-merge';
 
 import { Link, type LinkProps } from '@/components/Link';
 import BaseUILogo from '@/assets/images/logos/base-ui.svg?react';
@@ -15,7 +16,6 @@ import VitePlusLogo from '@/assets/images/logos/viteplus.svg?react';
 import VitePlusColorLogo from '@/assets/images/logos/viteplus-color.svg?react';
 import WarpLogo from '@/assets/images/logos/warp.svg?react';
 import ZedLogo from '@/assets/images/logos/zed.svg?react';
-import { twMerge } from 'tailwind-merge';
 
 export const Route = createFileRoute('/credits')({
   component: RouteComponent,
@@ -27,11 +27,73 @@ function RouteComponent() {
       <div className={'col-span-full pt-16 md:pt-0'}>
         <div className={'flex max-w-prose flex-col gap-6'}>
           <div className={'mb-10 flex flex-col gap-4'}>
-            <h1 className={'text-xl font-normal text-balance lg:text-2xl'}>Credits</h1>
-            {/*<p className={'text-secondary'}>
-              This website was made possible thanks to all the generous people who contributed to
-              these open-source projects:
-            </p>*/}
+            <h1 className={'text-xl text-balance lg:text-2xl'}>Credits</h1>
+            <p className={'text-balance text-secondary'}>
+              This website was made possible thanks to incredible open source projects and all the
+              generous people who contributed to them.
+            </p>
+          </div>
+
+          <div className={'flex flex-col gap-2'}>
+            <h2 className={'text-secondary'}>Tech stack</h2>
+            <ul className={'flex flex-col'}>
+              <Credit
+                name="Base UI"
+                url="https://base-ui.com"
+                icon={BaseUILogo}
+                brandColor="#03060D"
+              />
+              <Credit
+                name="tailwind-merge"
+                icon={NpmLogo}
+                brandColor="#cb3837"
+                url="https://www.npmjs.com/package/tailwind-merge"
+              />
+              <Credit
+                name="Tailwind CSS"
+                url="https://tailwindcss.com"
+                icon={TailwindCSSLogo}
+                brandColor="#38BDF8"
+              />
+              <Credit
+                name="Tanstack Start"
+                icon={TanstackLogo}
+                url="https://tanstack.com/start/latest"
+              />
+              <Credit name="React" url="https://react.dev" icon={ReactLogo} brandColor="#087ea4" />
+              <Credit
+                name="Vite Plus"
+                icon={VitePlusLogo}
+                hoverIcon={VitePlusColorLogo}
+                url="https://viteplus.dev"
+              />
+              <Credit
+                name="TypeScript"
+                url="https://www.typescriptlang.org/"
+                icon={TypeScriptLogo}
+                brandColor="#3178C6"
+              />
+              <Credit
+                name="Cloudflare Workers"
+                url="https://workers.cloudflare.com/product/workers/"
+                icon={CloudflareLogo}
+                brandColor="#FF4801"
+              />
+            </ul>
+          </div>
+
+          <div className={'flex flex-col gap-2'}>
+            <h2 className={'text-secondary'}>Tools</h2>
+            <ul className={'flex flex-col'}>
+              <Credit name="Zed" url="https://zed.dev" icon={ZedLogo} brandColor="#1348DC" />
+              <Credit name="Warp" url="https://www.warp.dev/" icon={WarpLogo} />
+              <Credit
+                name="Claude Code"
+                url="https://claude.com/product/claude-code"
+                icon={ClaudeLogo}
+                brandColor="#D97757"
+              />
+            </ul>
           </div>
 
           <div className={'flex flex-col gap-2'}>
@@ -49,74 +111,6 @@ function RouteComponent() {
                 icon={IBMPlexLogo}
                 brandColor="#054ada"
               />
-            </ul>
-          </div>
-
-          <div className={'flex flex-col gap-2'}>
-            <h2 className={'text-secondary'}>Front-end tooling</h2>
-            <ul className={'flex flex-col'}>
-              <Credit
-                name="Base UI"
-                url="https://base-ui.com"
-                icon={BaseUILogo}
-                brandColor="#03060D"
-              />
-              <Credit name="React" url="https://react.dev" icon={ReactLogo} brandColor="#087ea4" />
-              <Credit
-                name="Tailwind CSS"
-                url="https://tailwindcss.com"
-                icon={TailwindCSSLogo}
-                brandColor="#38BDF8"
-              />
-              <Credit
-                name="Tailwind Merge"
-                icon={NpmLogo}
-                brandColor="#cb3837"
-                url="https://www.npmjs.com/package/tailwind-merge"
-              />
-              <Credit
-                name="Tanstack Start"
-                icon={TanstackLogo}
-                url="https://tanstack.com/start/latest"
-              />
-              <Credit
-                name="TypeScript"
-                url="https://www.typescriptlang.org/"
-                icon={TypeScriptLogo}
-                brandColor="#3178C6"
-              />
-              <Credit
-                name="Vite Plus"
-                icon={VitePlusLogo}
-                hoverIcon={VitePlusColorLogo}
-                url="https://viteplus.dev"
-              />
-            </ul>
-          </div>
-
-          <div className={'flex flex-col gap-2'}>
-            <h2 className={'text-secondary'}>Hosting</h2>
-            <ul className={'flex flex-col'}>
-              <Credit
-                name="Cloudflare Workers"
-                url="https://workers.cloudflare.com/product/workers/"
-                icon={CloudflareLogo}
-                brandColor="#FF4801"
-              />
-            </ul>
-          </div>
-
-          <div className={'flex flex-col gap-2'}>
-            <h2 className={'text-secondary'}>Development</h2>
-            <ul className={'flex flex-col'}>
-              <Credit
-                name="Claude Code"
-                url="https://claude.com/product/claude-code"
-                icon={ClaudeLogo}
-                brandColor="#D97757"
-              />
-              <Credit name="Warp" url="https://www.warp.dev/" icon={WarpLogo} />
-              <Credit name="Zed" url="https://zed.dev" icon={ZedLogo} brandColor="#1348DC" />
             </ul>
           </div>
         </div>
@@ -139,12 +133,14 @@ function Credit({
   url: LinkProps['to'];
 }) {
   return (
-    <li className={'-ml-0.5 md:-ml-8'}>
+    <li>
       <Link to={url} className="group flex items-center gap-2 py-1.5">
-        <div className="flex size-6 items-center justify-center">
+        <div className="flex size-5 items-center justify-center">
           <Icon
             style={
-              { '--credit-color': brandColor ?? 'var(--color-neutral-950)' } as React.CSSProperties
+              {
+                '--credit-color': brandColor ?? 'var(--color-neutral-950)',
+              } as React.CSSProperties
             }
             className={twMerge(
               'size-full fill-neutral-400 group-hover:fill-(--credit-color) group-active:fill-(--credit-color)',
