2aa6022f485bb65f1f2ebac9ca1fe9340f96bb67
Nikita Pashinsky <nikitapashinsky@gmail.com>
2026-03-18 18:06:56 +0100

    add passthroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
@@ -8,17 +8,14 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [react()],
 
-  // TODO: remove once @astrojs/cloudflare includes the fix from
-  // https://github.com/withastro/astro/pull/15803
-  session: {
-    driver: { entrypoint: "unstorage/drivers/null" },
+  prefetch: true,
+  image: {
+    service: passthroughImageService(),
   },
-
   adapter: cloudflare(),
 });
