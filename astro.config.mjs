f065f34b0d533dc087791bd11480fcf74f232dca
Nikita Pashinsky <nikitapashinsky@gmail.com>
2026-03-19 09:15:36 +0100

    Revert "move to next.js"
    This reverts commit 11141f89e45c1709b7d780576a63669a2e469953.

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react()],
+  adapter: cloudflare(),
+});
---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+import { defineCollection } from "astro:content";
+
+import { file } from "astro/loaders";
+
+import { z } from "astro/zod";
+
+const work = defineCollection({
+  loader: file("src/data/work.json"),
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    company: z.string(),
+    product: z.string(),
+    year: z.string(),
+    src: z.string(),
+    width: z.number(),
+    height: z.number(),
+  }),
+});
+
+export const collections = { work };
