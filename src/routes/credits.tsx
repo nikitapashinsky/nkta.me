606487834ae4aaa72b2b15ec872cd963e0b9f575
Nikita Pashinsky <nikitapashinsky@gmail.com>
2026-04-09 00:52:05 +0200

    improve /credits styling

--- src/routes/credits.tsx
+++ src/routes/credits.tsx
@@ -20,6 +20,7 @@ function RouteComponent() {
       className={twJoin(
         'col-span-full row-start-2',
         'grid grid-cols-subgrid content-start gap-x-4 gap-y-16',
+        'md:pb-16',
       )}
     >
       <div className={'col-span-full flex flex-col gap-4 lg:gap-6'}>
@@ -32,9 +33,9 @@ function RouteComponent() {
 
       <div className={'col-span-full grid grid-cols-subgrid gap-8'}>
         {/* Tech stack */}
-        <div className={twJoin('col-span-full grid grid-cols-subgrid gap-2')}>
-          <h2 className={'col-span-full text-secondary'}>Tech stack</h2>
-          <ul className={'col-span-full grid grid-cols-subgrid gap-x-4 gap-y-2'}>
+        <div className={twJoin('col-span-full grid grid-cols-subgrid gap-4')}>
+          <h3 className={'col-span-full font-medium text-secondary'}>Tech stack</h3>
+          <ul className={'col-span-full grid grid-cols-subgrid gap-x-4 gap-y-1 md:gap-y-2'}>
             {tech.map(({ name, icon, url }) => (
               <Credit
                 key={name}
@@ -45,6 +46,7 @@ function RouteComponent() {
                 iconClassName={twJoin(
                   'rounded-full bg-white p-2.5 shadow-md shadow-black/4 outline-[0.5px] outline-black/8',
                   'group-hover:shadow-black/8 group-hover:outline-black/10',
+                  'group-active:shadow-black/8 group-active:outline-black/10',
                   'transition-all duration-125 group-hover:duration-75',
                 )}
               />
@@ -53,9 +55,9 @@ function RouteComponent() {
         </div>
 
         {/* Tools */}
-        <div className={twJoin('col-span-full grid grid-cols-subgrid gap-2')}>
-          <h2 className={'col-span-full text-secondary'}>Tools</h2>
-          <ul className={'col-span-full grid grid-cols-subgrid gap-x-4 gap-y-2'}>
+        <div className={twJoin('col-span-full grid grid-cols-subgrid gap-4')}>
+          <h2 className={'col-span-full font-medium text-secondary'}>Tools</h2>
+          <ul className={'col-span-full grid grid-cols-subgrid gap-x-4 gap-y-1 md:gap-y-2'}>
             {tools.map(({ name, description, icon, url }) => (
               <Credit
                 key={name}
@@ -64,7 +66,19 @@ function RouteComponent() {
                 icon={icon}
                 url={url}
                 className="col-span-1 md:col-span-4"
-                iconClassName="overflow-clip rounded-xl"
+                containerClassName={twJoin(
+                  'rounded-[18px] supports-corner-shape:rounded-[19px] supports-corner-shape:corner-smooth',
+                  'md:rounded-[20.5px] md:supports-corner-shape:rounded-[22px]',
+                )}
+                iconClassName={twMerge(
+                  'relative size-9.5 place-self-center overflow-clip rounded-[10px] supports-corner-shape:rounded-[11px] supports-corner-shape:corner-smooth',
+                  'shadow outline-[0.5px] outline-black/8',
+                  'md:size-11.5 md:rounded-[12.5px] md:supports-corner-shape:rounded-[14px]',
+                  'group-hover:shadow-md group-hover:shadow-black/12 group-hover:outline-black/10',
+                  'after:absolute after:inset-[0.5px] after:bg-linear-to-b after:from-white/15 after:from-4% after:to-white/0 after:to-70%',
+                  'after:rounded-[9.5px] md:after:rounded-[12px] after:supports-corner-shape:rounded-[10.5px] after:supports-corner-shape:corner-smooth md:after:supports-corner-shape:rounded-[13.5px]',
+                  name !== 'Codex' && '-outline-offset-[0.5px]',
+                )}
               />
             ))}
           </ul>
@@ -98,6 +112,7 @@ function Credit({
   description,
   url,
   className,
+  containerClassName,
   iconClassName,
 }: {
   icon: React.FC<React.SVGProps<SVGSVGElement>> | string;
@@ -107,19 +122,26 @@ function Credit({
   description?: string;
   url: string;
   className?: string;
+  containerClassName?: string;
   iconClassName?: string;
 }) {
   return (
     <li className={className}>
       <a
         href={url}
-        className={twJoin(
-          'group -ml-2 flex items-center gap-3 rounded-xl p-2',
-          'transition-colors duration-125 hover:bg-neutral-100 hover:duration-75',
+        className={twMerge(
+          'group -ml-2 grid items-center gap-3 rounded-xl p-2',
+          'grid-cols-[40px_1fr] md:grid-cols-[48px_1fr]',
+          'hover:bg-neutral-100 active:bg-neutral-100',
+          'transition-colors duration-125 hover:duration-75',
+          containerClassName,
         )}
       >
         <div
-          className={twMerge('flex size-12 shrink-0 items-center justify-center', iconClassName)}
+          className={twMerge(
+            'flex size-10 shrink-0 items-center justify-center md:size-12',
+            iconClassName,
+          )}
         >
           <Icon className={'size-full shrink-0'} />
         </div>
